7306f30d9b4c94ca0b2284023667f62daf88fc6f
Учитель <lp.pavli@gmail.com>
2023-12-16 16:23:51 +0300

    добавлена функция print_hello

--- main.py
+++ main.py
@@ -2,5 +2,9 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
+def print_hello(name):
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
 if __name__ == '__main__':
     print_hi('PyCharm')
